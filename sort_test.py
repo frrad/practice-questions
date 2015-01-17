664669f240427c962d90bf33c9b7c54dba3b1bc8
Frederick Robinson <frederick.robinson@gmail.com>
2015-01-16 16:10:05 -0800

    better tests

--- sort_test.py
+++ sort_test.py
@@ -1,14 +1,24 @@
 import sort
 import unittest
 
-class TestSort(unittest.TestCase):
-  def test_empty(self):
-    self.assertEqual(sort.sort([]), [])
 
-  def test_backwards(self):
-    backwards = reversed(range(10))
-    self.assertEqual(sort.sort(backwards),range(10))
+class TestSort(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual(sort.sort([]), [])
+
+    def test_backwards(self):
+        test_list = list(reversed(range(10)))
+        output = sorted(test_list)
+
+        self.assertEqual(sort.sort(test_list), output)
+
+    def test_random(self):
+        test_list = [1, 5, 6, 7, 8, 3, 20, -10, 1, 2345, 3, 14]
+        output = sorted(test_list)
+
+        self.assertEqual(sort.sort(test_list), output)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
